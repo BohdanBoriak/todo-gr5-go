dc91fcbc69eb93223dfa638f57e143b69329d995
BohdanBoriak <lightraven72@gmail.com>
2024-05-23 09:52:06 +0300

    Added: endpoint Save

--- internal/infra/http/requests/task_request.go
+++ internal/infra/http/requests/task_request.go
@@ -7,15 +7,15 @@ import (
 )
 
 type TaskRequest struct {
-	Title       string  `json:"title" validate:"required"`
-	Description *string `json:"description"`
-	Deadline    int64   `json:"deadline" validate:"required"`
+	Title       string `json:"title" validate:"required"`
+	Description string `json:"description"`
+	Deadline    int64  `json:"deadline" validate:"required"`
 }
 
 func (r TaskRequest) ToDomainModel() (interface{}, error) {
 	return domain.Task{
 		Title:       r.Title,
-		Description: *r.Description,
+		Description: r.Description,
 		Deadline:    time.Unix(r.Deadline, 0),
 	}, nil
 }
